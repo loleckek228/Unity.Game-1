062872b075936c9dac935dff63cb4ad815ecce3f
Loleckek228 <george-valiev@yandex.ru>
2019-11-05 04:34:45 +0300

    no message

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+	[SerializeField] private Animator AuthorsAnimator;
+
+    public void Play()
+    {
+        SceneManager.LoadScene("level1");
+    }
+
+    public void Authors()
+    {
+        AuthorsAnimator.SetBool("isOpen", !AuthorsAnimator.GetBool("isOpen"));
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }    
+}
---
+++ Assets/Scripts/RemoveObjects.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveObjects : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Gold"))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Obstacles"))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
