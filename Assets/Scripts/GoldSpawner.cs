912776390b83bd22b14c0808a8ea261c86364476
Loleckek228 <george-valiev@yandex.ru>
2019-12-26 16:46:03 +0300

    не придумал как не дублировать yield return в GoldSpawner

---
+++ Assets/Scripts/GoldSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GoldSpawner : Spawner
+{
+    private void Start()
+    {
+        StartCoroutine(DelayBetweenGoldSpawn());
+    }
+
+    private IEnumerator DelayBetweenGoldSpawn()
+    {
+        while (true)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                yield return new WaitForSeconds(0.2f);
+                Spawn();
+            }
+            yield return new WaitForSeconds(5);
+        }
+    }
+}
--- Assets/Scripts/ScrollingBackground.cs
+++ Assets/Scripts/ScrollingBackground.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Renderer))]
+
 public class ScrollingBackground : MonoBehaviour
 {
     private Renderer _renderer;
